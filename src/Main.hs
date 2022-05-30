4c0ff084336621256d13a9571cccf5d3ff86eff9
CNoctem <gergely@fastmail.jp>
2022-05-30 15:59:33 +0200

    .

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,6 @@ module Main where
 
 import Web.Scotty
 import Network.HTTP.Types (status418)
---import Web.Scotty.Resource.Trans (resource, get, post)
---import Web.Scotty.Trans (scottyT, text, body, raw, status, param)
 
 main :: IO ()
 main = do
