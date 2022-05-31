1268c06b1a104c3bb4fef97e75bee929b35ccbcd
CNoctem <gergely@fastmail.jp>
2022-05-31 11:52:19 +0200

    + /

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,12 @@ main :: IO ()
 main = do
 
   scotty 8000 $ do
+    get "/" $ do
+      status status418
+      text "root\n"
     get "/status" $ do
       status status418
       text "Everything is hunky-dory.\n"
+    
 
 
