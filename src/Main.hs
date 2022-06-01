7566dad520a313d31293932acac9f6edd2ba4c1f
CNoctem <gergely@fastmail.jp>
2022-06-01 11:35:59 +0200

    added info -> json

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,39 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# OPTIONS_GHC -fno-cse #-}
+
+
 module Main where
 
-import Web.Scotty
+import Web.Scotty as S
 import Network.HTTP.Types (status418)
+import System.Console.CmdArgs
+import GHC.Generics
+import Data.Aeson
 
 main :: IO ()
+
+data Config = Config { port :: Int
+                     , nodeId :: String
+                     , nodeName :: String
+                     } deriving (Show, Data, Typeable, Generic )
+
+config :: Config
+config = Config { port = 8000, nodeId = "NID01", nodeName = "NN0A1" }
+
+instance ToJSON Config
+
 main = do
 
-  scotty 8000 $ do
+  cnf <- cmdArgs config
+  
+  scotty (port cnf) $ do
     get "/" $ do
       status status418
       text "root\n"
     get "/status" $ do
       status status418
-      text "Everything is hunky-dory.\n"
-    
-
-
+      text "Everything is hunky-dory."
+    get "/info" $ do
+      status status418
+      S.json cnf
